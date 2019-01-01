ecb3c422f8d1a795383ad0267fbaf70009ddaac0
Carror Deng <carrordeng@gmail.com>
2019-01-01 18:53:38 +0800

    ex-1.9

--- ch01/ex-1.9.cpp
+++ ch01/ex-1.9.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int sum=0, val=50;
-    while(val<=100)
+    int sum = 0, val = 50;
+    while (val <= 100)
     {
-        sum+=val;
+        sum += val;
         val++;
     }
     std::cout << "Sum of 50 to 100 inclusive is "
-              << sum <<std::endl;
+              << sum << std::endl;
     return 0;
 }
