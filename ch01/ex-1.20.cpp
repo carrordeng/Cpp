4666f9a43740733feb17f6763bb5c8cf013781da
Carror Deng <carrordeng@gmail.com>
2019-03-06 19:41:01 +0800

    change return value

--- ch01/ex-1.20.cpp
+++ ch01/ex-1.20.cpp
@@ -10,11 +10,12 @@ int main()
         while (std::cin >> item)
         {
             std::cout << item;
-        }
+    }
+        return 0;
     }
     else
     {
         std::cout << "No input?" << std::endl;
+        return -1;
     }
-    return 0;
 }
