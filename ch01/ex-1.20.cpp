ff152936949bcb37e685b3839d9cb7a8e5f3f284
Carror Deng <carrordeng@gmail.com>
2019-03-06 19:15:48 +0800

    ex-1.20

---
+++ ch01/ex-1.20.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "../include/Sales_item.h"
+
+int main()
+{
+    Sales_item item;
+    if(std::cin >> item)
+    {
+        std::cout << item;
+        while(std::cin >> item)
+        {
+            std::cout << item;
+        }
+    }
+    else
+    {
+        std::cout << "No input?" << std::endl;
+    }
+    return 0;
+}
