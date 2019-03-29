4361b3d3bef8dc786de26b347c48278191508936
carrordeng <carror1994@gmail.com>
2019-03-29 20:13:17 +0800

    ex-1.23

---
+++ ch01/ex-1.23.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "../include/Sales_item.h"
+
+int main()
+{
+    Sales_item curritem, item;
+    if (std::cin >> curritem)
+    {
+        int cnt = 1;
+        while (std::cin >> item)
+        {
+            if(item.isbn() == curritem.isbn())
+                cnt++;
+            else
+            {
+                std::cout << "For ISBN " << curritem.isbn() << " transaction occured " << cnt << " times" << std::endl;
+                cnt = 1;
+                curritem = item;
+            }
+            std::cout << "For ISBN " << curritem.isbn() << " transaction occured " << cnt << " times" << std::endl;
+        }
+    }
+    else
+    {
+        std::cerr << "No input data?" << std::endl;
+        return -1;
+    }
+}
