4b61ca20cd4c5a0b86d35dfe75f9b860667134e5
carrordeng <carror1994@gmail.com>
2019-03-30 15:16:56 +0800

    add unsigned int

--- ch02/ex-2.41.1.cpp
+++ ch02/ex-2.41.1.cpp
@@ -5,7 +5,7 @@ struct Sales_data
 {
     std::string bookNo;
     std::string bookName;
-    unsigned units_sold = 0;
+    unsigned int units_sold = 0;
     double price = 0.0;
     double revenue = 0.0;
 };
