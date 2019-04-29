051b5472bd659983aa7194a739f37f02e8d03e4e
carrordeng <carror1994@gmail.com>
2019-04-29 15:01:12 +0800

    ex-3.31

--- ch03/ex-3.31.cpp
+++ ch03/ex-3.31.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+using std::cout;
+
 int main()
 {
     int array[10];
     for (size_t i = 0; i < 10; ++i)
+    {
         array[i] = i;
+        cout << array[i] << " ";
+    }
     return 0;
 }
