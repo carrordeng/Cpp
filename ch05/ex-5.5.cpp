b859c452cf2f337b79a8fcb4b49c9f9384042581
carrordeng <carror1994@gmail.com>
2019-05-03 13:05:31 +0800

    ex-5.5

---
+++ ch05/ex-5.5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include "string"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+    const vector<string> scores = {"F", "D", "C", "B", "A", "A++"};
+    int grade;
+    while (cin >> grade)
+    {
+        string lettergrade;
+        if (grade < 60)
+            lettergrade = scores[0];
+        else
+        {
+            lettergrade = scores[(grade - 50) / 10];
+            if (grade != 100)
+            {
+                if (grade % 10 > 7)
+                    lettergrade += "+";
+                else if (grade % 10 < 3)
+                    lettergrade += "-";
+            }
+        }
+        cout << lettergrade << endl;
+    }
+    return 0;
+}
