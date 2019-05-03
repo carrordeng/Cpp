dbf351157a51a697c38d971194812315fa400446
carrordeng <carror1994@gmail.com>
2019-05-03 16:14:53 +0800

    ex-5.11

---
+++ ch05/ex-5.11.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    unsigned aCnt = 0, eCnt = 0, iCnt = 0, oCnt = 0, uCnt = 0,
+             bsCnt = 0, tabCnt = 0, nlCnt = 0;
+    char ch;
+    while (cin >> ch)
+    {
+        switch (ch)
+        {
+        case 'A':
+        case 'a':
+            ++aCnt;
+            break;
+        case 'E':
+        case 'e':
+            ++eCnt;
+            break;
+        case 'I':
+        case 'i':
+            ++iCnt;
+            break;
+        case 'O':
+        case 'o':
+            ++oCnt;
+            break;
+        case 'U':
+        case 'u':
+            ++uCnt;
+            break;
+        case ' ':
+            ++bsCnt;
+            break;
+        case '\t':
+            ++tabCnt;
+            break;
+        case '\n':
+            ++nlCnt;
+            break;
+        }
+    }
+    cout << "Number of vowel a: \t" << aCnt << endl
+         << "Number of vowel e: \t" << eCnt << endl
+         << "Number of vowel i: \t" << iCnt << endl
+         << "Number of vowel o: \t" << oCnt << endl
+         << "Number of vowel u: \t" << uCnt << endl
+         << "Number of blank spaces: \t" << bsCnt << endl
+         << "Number of tabs: \t" << tabCnt << endl
+         << "Number of newlines: \t" << nlCnt << endl;
+    return 0;
+}
