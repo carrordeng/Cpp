4d5a4879d5f324330821b8af3cea1fe88d6ceb22
carrordeng <carror1994@gmail.com>
2019-05-05 10:00:51 +0800

    ex-5.23

---
+++ ch05/ex-5.23.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int num1, num2;
+    cin >> num1 >> num2;
+    cout << static_cast<double>(num1) / num2 << endl;
+    return 0;
+}
