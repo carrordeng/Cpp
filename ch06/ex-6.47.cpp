be0390f1f020ea2f38d9118fffc1c70b4367f70a
carrordeng <carror1994@gmail.com>
2019-05-12 15:56:23 +0800

    ex-6.47

---
+++ ch06/ex-6.47.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+void print(vector<int>::iterator beg, vector<int>::iterator end)
+{
+    if (beg != end)
+    {
+        cout << *beg << " ";
+#ifndef NDEBUG
+        cout << end - beg << endl;
+#endif
+        print(++beg, end); //print(std::next(beg), end);
+    }
+}
+
+int main()
+{
+    vector<int> ivec{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    print(ivec.begin(), ivec.end());
+    return 0;
+}
