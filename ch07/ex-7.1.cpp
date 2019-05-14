decb20b8e55fabd0de8283ecbd0125269933e2bd
carrordeng <carror1994@gmail.com>
2019-05-14 10:18:07 +0800

    ex-7.1

---
+++ ch07/ex-7.1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "../ch02/Sales_data.h"
+
+using std::cerr;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    Sales_data total;
+    if (cin >> total.bookNo >> total.bookName >> total.units_sold >> total.price)
+    {
+        total.CalRevenue();
+        Sales_data trans;
+        while (cin >> trans.bookNo >> trans.bookName >> trans.units_sold >> trans.price)
+        {
+            trans.CalRevenue();
+            if (total.bookNo == trans.bookNo)
+                total.AddData(trans);
+            else
+            {
+                total.Print();
+                total.SetData(trans);
+            }
+        }
+        total.Print();
+    }
+    else
+    {
+        cerr << "No data?" << endl;
+        return -1;
+    }
+    return 0;
+}
