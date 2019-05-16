7e7aae72cf9905934ff54ef2b9405483625af66f
carrordeng <carror1994@gmail.com>
2019-05-16 13:49:59 +0800

    ex-7.1

---
+++ ch07/Sales_data.h
@@ -0,0 +1,52 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include <string>
+#include <iostream>
+
+struct Sales_data
+{
+    std::string bookNo;
+    std::string bookName;
+    unsigned int units_sold;
+    double price = 0.0;
+    double revenue = 0.0;
+
+    void CalRevenue();
+    void Print();
+    void AddData(Sales_data data);
+    void SetData(Sales_data data);
+};
+
+void Sales_data::CalRevenue()
+{
+    revenue = price * units_sold;
+}
+
+void Sales_data::Print()
+{
+    std::cout << bookNo << " "
+              << bookName << " "
+              << units_sold << " "
+              << price << " "
+              << revenue << std::endl;
+}
+
+void Sales_data::AddData(Sales_data data)
+{
+    if (bookNo != data.bookNo)
+        return;
+    units_sold += data.units_sold;
+    revenue += data.revenue;
+}
+
+void Sales_data::SetData(Sales_data data)
+{
+    bookNo = data.bookNo;
+    bookName = data.bookName;
+    units_sold = data.units_sold;
+    price = data.price;
+    revenue = data.revenue;
+}
+
+#endif
