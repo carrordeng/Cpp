9745fec63630cbd79aa68297daf416a51dc66556
carrordeng <carror1994@gmail.com>
2019-05-18 10:43:05 +0800

    ex-7.24

---
+++ ch07/ex-7.24.h
@@ -0,0 +1,22 @@
+#ifndef EX_7_24_H
+#define EX_7_24_H
+
+#include <string>
+
+class Screen
+{
+public:
+    typedef std::string::size_type pos;
+
+private:
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    std::string contents;
+
+public:
+    Screen() = default;
+    Screen(const pos &h, const pos &w, const char &c) : height(h), width(w), contents(h * w, c) {}
+    inline const char get() const { return contents[cursor]; }
+    inline const char get(const pos &row, const pos &col) const { return contents[row * width + col]; }
+};
+#endif
