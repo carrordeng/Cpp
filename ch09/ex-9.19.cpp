8edb3ff7b68baf5ad7fc4de2925061e881d806f3
carrordeng <carror1994@gmail.com>
2019-07-06 17:56:14 +0800

    ex-9.19

---
+++ ch09/ex-9.19.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::list;
+using std::string;
+
+int main()
+{
+    list<string> slist;
+    string input;
+    while (cin >> input)
+        slist.push_back(input);
+    for (auto it = slist.cbegin(); it != slist.cend(); ++it)
+        cout << *it << endl;
+}
