456a62c90dd3a23c99de81f433ed0c63b6d3a179
carrordeng <carror1994@gmail.com>
2019-07-06 18:14:28 +0800

    ex-9.20

---
+++ ch09/ex-9.20.1.cpp
@@ -0,0 +1,27 @@
+#include <deque>
+#include <iostream>
+#include <list>
+
+using std::cout;
+using std::deque;
+using std::endl;
+using std::list;
+
+int main()
+{
+    list<int> ilist{1, 2, 4, 5, 7, 8, 11};
+    deque<int> even_que, odd_que;
+    for (auto it = ilist.cbegin(); it != ilist.cend(); ++it)
+    {
+        if (*it % 2 == 0)
+            even_que.push_back(*it);
+        else
+            odd_que.push_back(*it);
+    }
+    for (auto it = even_que.cbegin(); it != even_que.cend(); ++it)
+        cout << *it << " ";
+    cout << endl;
+    for (auto it = odd_que.cbegin(); it != odd_que.cend(); ++it)
+        cout << *it << " ";
+    cout << endl;
+}
