d07afcfcfa0df61c9f97d69d0989bc905594a0c0
carrordeng <carror1994@gmail.com>
2019-07-07 11:06:38 +0800

    ex-9.26

---
+++ ch09/ex-9.26.1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::list;
+using std::vector;
+
+int main()
+{
+    int ia[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 55, 89};
+    vector<int> ivec;
+    list<int> ilist;
+    for (auto i : ia)
+    {
+        ivec.push_back(i);
+        ilist.push_back(i);
+    }
+    for (auto iter = ilist.begin(); iter != ilist.end(); ++iter)
+    {
+        if (*iter % 2 == 1)
+            ilist.erase(iter);
+    }
+    for (auto i : ilist)
+        cout << i << " ";
+    cout << endl;
+    for (auto iter = ivec.begin(); iter != ivec.end(); ++iter)
+    {
+        if (*iter % 2 == 0)
+            ivec.erase(iter);
+    }
+    for (auto i : ivec)
+        cout << i << " ";
+    cout << endl;
+}
