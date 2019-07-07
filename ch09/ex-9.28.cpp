02cd3980b551dbff8046efc2cf30dd997855f162
carrordeng <carror1994@gmail.com>
2019-07-07 12:32:19 +0800

    ex-9.28

---
+++ ch09/ex-9.28.cpp
@@ -0,0 +1,40 @@
+#include <forward_list>
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::forward_list;
+using std::string;
+
+void insertString(forward_list<string> &sflist, const string &str1, const string &str2)
+{
+    auto prev = sflist.before_begin();
+    auto curr = sflist.begin();
+    bool found = false;
+    while (curr != sflist.end())
+    {
+        if (*curr == str1)
+        {
+            curr = ++sflist.insert_after(curr, str2);
+            prev++;
+            prev++;
+            found = true;
+        }
+        else
+            prev = curr++;
+    }
+    if (!found)
+        sflist.insert_after(prev, str2);
+}
+
+int main()
+{
+    forward_list<string> sflist{"a", "bb", "ccc", "bb", "dddd"};
+    insertString(sflist, "a", "a");
+    insertString(sflist, "bb", "bb");
+    insertString(sflist, "???", "???");
+    for (auto str : sflist)
+        cout << str << " ";
+    cout << endl;
+}
